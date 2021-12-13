94b6a9ef722259b4609afb2e47ed4ca93bc7f322
Mao Han <han_mao@linux.alibaba.com>
2021-12-13 14:10:44 +0800

    Add T-HEAD specific ld/compile/optimize support
    Merged by following commits:
        generic/typesizes.h: Add support for 32-bit arches with 64-bit types
        RISCV: add config of thead
        RISCV: add memcpy/memmove/strcmp of thead
        RISC-V: Fix bad parsing about __riscv_xtheadc.
        [RISC-V] Support memcpy.S with rvv1.0
        [RISC-V] Support memcpy.S with rvv1.0, adapt to errors due to -Werror=undef.
        [RISC-V][THEAD] Add v0p7 for multilib.
        [T-HEAD] Add ldso for lp64dv without xthead
        [T-HEAD][RISCV] Support rv32imafdcv_zfh_xtheadc-ilp32d

--- sysdeps/unix/sysv/linux/riscv/ldconfig.h
+++ sysdeps/unix/sysv/linux/riscv/ldconfig.h
@@ -22,13 +22,31 @@
 #define LD_SO_SUFFIX ".so.1"
 
 #if __riscv_xlen == 64
+#if defined(__riscv_xthead) && defined(__riscv_v)
+#if __riscv_v >= 1000000
+# define LD_SO_ABI "riscv64v_xthead-lp64"
+#else
+# define LD_SO_ABI "riscv64v0p7_xthead-lp64"
+#endif
+#elif defined(__riscv_xthead)
+# define LD_SO_ABI "riscv64xthead-lp64"
+#else
 # define LD_SO_ABI "riscv64-lp64"
+#endif
+#else
+#if defined(__riscv_xthead) && defined(__riscv_v)
+# define LD_SO_ABI "riscv32v_xthead-ilp32"
+#elif defined(__riscv_xthead)
+# define LD_SO_ABI "riscv32xthead-ilp32"
 #else
 # define LD_SO_ABI "riscv32-ilp32"
 #endif
+#endif
 
 #define SYSDEP_KNOWN_INTERPRETER_NAMES				\
+  { LD_SO_PREFIX LD_SO_ABI "v" LD_SO_SUFFIX, FLAG_ELF_LIBC6 },  \
   { LD_SO_PREFIX LD_SO_ABI "d" LD_SO_SUFFIX, FLAG_ELF_LIBC6 },	\
+  { LD_SO_PREFIX LD_SO_ABI "dv" LD_SO_SUFFIX, FLAG_ELF_LIBC6 }, \
   { LD_SO_PREFIX LD_SO_ABI     LD_SO_SUFFIX, FLAG_ELF_LIBC6 },
 
 #define SYSDEP_KNOWN_LIBRARY_NAMES	\
